3610d84548611bcd005b306799671e192a54b159
Vinícius Silva Santos <vinicius01012@gmail.com>
2024-07-17 08:53:51 -0300

    Update customers.module.ts
    I removed an unused import

--- src/petshop/customers/customers.module.ts
+++ src/petshop/customers/customers.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { CustomersService } from './customers.service';
 import { CustomersController } from './customers.controller';
-import { PrismaModule } from 'src/plugins/prisma.module';
 
 @Module({
   controllers: [CustomersController],
