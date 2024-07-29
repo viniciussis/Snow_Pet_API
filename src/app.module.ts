6e01cdcb5cd6695be7e744d5e536b4db799fa2cc
Vinícius Silva Santos <vinicius01012@gmail.com>
2024-07-28 22:34:33 -0300

    Improve folder arquitechture

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
-import { CategoriesModule } from './petshop/categories/categories.module';
-import { CustomersModule } from './petshop/customers/customers.module';
-import { GroomingsModule } from './petshop/groomings/groomings.module';
-import { ServicesModule } from './petshop/services/services.module';
-import { ProductsModule } from './petshop/products/products.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { CustomersModule } from './modules/customers/customers.module';
+import { GroomingsModule } from './modules/groomings/groomings.module';
+import { ServicesModule } from './modules/services/services.module';
+import { ProductsModule } from './modules/products/products.module';
 import { PrismaModule } from './plugins/database/database.module';
-import { StockModule } from './petshop/stock/stock.module';
-import { PetsModule } from './petshop/pets/pets.module';
+import { StockModule } from './modules/stock/stock.module';
+import { PetsModule } from './modules/pets/pets.module';
 import { Module } from '@nestjs/common';
 
 @Module({
